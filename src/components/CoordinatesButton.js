16af694547a39e7d502051ba12bbbef8406659cc
github username <raquel.sae.randall@gmail.com>
2019-01-10 21:55:39 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+import onReceiveCoordinates from '../index'
+
+class CoordinatesButton extends React.Component {
+
+
+  render(){
+    return(
+      <button
+        onClick={(event) => this.props.onReceiveCoordinates([event.clientX, event.clientY])}>
+        Click
+      </button>
+    )
+  }
+}
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react';
+import onReceiveCoordinates from '../index'
+
+class DelayedButton extends React.Component {
+
+  render(){
+    return(
+      <button
+      onClick={(event) => setTimeout(()=> {
+        this.props.onDelayedClick(event);
+      }, this.props.delay)}
+      >
+        Click
+      </button>
+    )
+  }
+}
+export default DelayedButton;
